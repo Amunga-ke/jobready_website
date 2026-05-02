9fa83dfc5079086fe7e64459a3c4f1a1d2893125
Z User <z@container>
2026-05-02 10:27:57 +0000

    b8093c44-958f-4b26-b055-a66b82479a92

--- src/components/fursa/JobDetailSheet.tsx
+++ src/components/fursa/JobDetailSheet.tsx
@@ -174,7 +174,7 @@ export default function JobDetailSheet() {
   const deadlineText = daysUntil(job.deadline);
 
   return (
-    <div className="fixed inset-0 z-50">
+    <div className="fixed inset-0 z-[60]" aria-modal="true" role="dialog">
       {/* Backdrop */}
       <div
         ref={backdropRef}
